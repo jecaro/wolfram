1e3edbdcc12566fde6e11765e554d807c18288ba
jecaro <jeancharles.quillet@gmail.com>
2026-01-08 08:13:30 +0100

    Bump to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
